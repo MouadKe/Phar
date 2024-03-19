95bf8630ed9ea1082eb0f826e045c2ad0844a879
MouadKe <mouadkennouche@gmail.com>
2024-03-19 20:29:23 +0100

    made the code more readable and secure and more user friendly

--- phar.c
+++ phar.c
@@ -25,13 +25,26 @@ typedef struct node {
     struct node *next;
 }node;
 
+// Function prototypes
+// data structure functions
 node** init();
 node* createNode(med data);
+
+// input/output functions
 void readmed(med* m);
-bool expiring(med m);
-void Textform(char* N);
 void fill(node** array,med m);
 void PrintStock(const node** array);
+bool Badnumber(int N);
+
+// string functions
+void Textform(char* N);
+bool BadName(char N);
+
+//date functions
+bool expiring(med m);
+bool BadDate(date d , bool manf);
+bool Manfucdate(date manf,date exp);
+
 
 
 int main(){
@@ -65,53 +78,69 @@ node* createNode(med data){
     return temp;
 }
 
+
+
 void readmed(med* m){
+    int counter =0;
+
+
     printf("Enter brand: ");
     scanf("%s",m->brand);
     Textform(m->brand);
 
-    printf("Enter name: ");
-    scanf("%s",m->name);
-    Textform(m->name);
-
-    printf("Enter quantity: ");
-    scanf("%d",&m->quantity);
-
-    printf("Enter price: ");
-    scanf("%f",&m->price);
-
-    printf("Enter manufacturing date: ");
-    scanf("%d %d %d",&m->manf.day,&m->manf.month,&m->manf.year);
-
-    printf("Enter expiry date: ");
-    scanf("%d %d %d",&m->exp.day,&m->exp.month,&m->exp.year);
-}
-
-bool expiring(med m){
-    time_t t = time(NULL);
-  struct tm tm = *localtime(&t);
-    if(m.exp.year<tm.tm_year + 1900){
-        return true;
-    }
-    else if(m.exp.year==tm.tm_year + 1900){
-        if(m.exp.month<tm.tm_mon + 1){
-            return true;
+    do{
+        if(counter>0){
+            printf("Invalid name!!!\nThe name should start with a letter\n");
         }
-        else if(m.exp.month==tm.tm_mon + 1){
-            if(m.exp.day<tm.tm_mday){
-                return true;
+        printf("Enter name: ");
+        scanf("%s",m->name);
+        Textform(m->name);
+        counter++;
+
+    }while(BadName(m->name[0]));
+    counter = 0;
+    
+    do{
+        if(counter>0){
+            printf("Invalid quantity!!!\nQuantity should be a positive number\n");
+        }
+        printf("Enter quantity: ");
+        scanf("%d",&m->quantity);
+        counter++;
+    }while(Badnumber(m->quantity));
+    counter = 0;
+
+    do{
+        if(counter>0){
+            printf("Invalid price!!!\nPrice should be a positive number\n");
+        }
+        printf("Enter price: ");
+        scanf("%f",&m->price);
+        counter++;
+    }while(Badnumber(m->price));
+    counter = 0;
+
+    do{
+        if(counter>0){
+            printf("Invalid manufacturing date!!!\n");
+        }
+        printf("Enter manufacturing date: ");
+        scanf("%d %d %d",&m->manf.day,&m->manf.month,&m->manf.year);
+        counter++;
+    }while(BadDate(m->manf,true));
+    counter = 0;
+
+    do{
+        if(counter>0){
+            printf("Invalid expiry date!!!\n");
+            if(Manfucdate(m->manf,m->exp)){
+                printf("Expiry date should be after the manufacturing date\n");
             }
         }
-    }
-    return false;
-}
-
-void Textform(char* N){
-    N[0] = toupper(N[0]);
-    for(int i = 1;N[i]!='\0';i++){
-        N[i] = tolower(N[i]);
-    }
-
+        printf("Enter expiry date (dd mm yyyy): ");
+        scanf("%d %d %d",&m->exp.day,&m->exp.month,&m->exp.year);
+        counter++;
+    }while(BadDate(m->exp,false) || Manfucdate(m->manf,m->exp));
 }
 
 void fill(node** array,med m){
@@ -146,5 +175,77 @@ void PrintStock(const node** array){
                 temp = temp->next;
             }
         }
+    }
+}
+
+bool Badnumber(int N){
+    if(N<0) return true;
+    return false;
+}
+
+
+void Textform(char* N){
+    N[0] = toupper(N[0]);
+    for(int i = 1;N[i]!='\0';i++){
+        N[i] = tolower(N[i]);
+    }
+
+}
+
+bool BadName(char N){
+    if(N>='A' && N<='Z') return false;
+    return true;
+}
+
+
+
+bool Manfucdate(date manf,date exp){
+    if(exp.year<manf.year) return true;
+    else if(exp.year==manf.year){
+        if(exp.month<manf.month) return true;
+        else if(exp.month==manf.month){
+            if(exp.day<manf.day) return true;
+        }
+    }
+    return false;
+}
+
+bool expiring(med m){
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    if(m.exp.year<tm.tm_year + 1900){
+        return true;
+    }
+    else if(m.exp.year==tm.tm_year + 1900){
+        if(m.exp.month<tm.tm_mon + 1){
+            return true;
+        }
+        else if(m.exp.month==tm.tm_mon + 1){
+            if(m.exp.day<tm.tm_mday){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool BadDate(date d , bool manf){
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    if(manf && d.year>tm.tm_year + 1900) return true;
+    if(d.year<1) return true;
+    if (d.month < 1 || d.month > 12) return true;
+    if (d.day < 1 || d.day > 31) return true;
+    switch(d.month){
+        case 4:case 6:case 9:case 11:
+            if(d.day>30) return true;
+            break;
+        case 2:
+            if(d.year%4==0){
+                if(d.day>29) return true;
+            }else{
+                if(d.day>28) return true;
+            }
+            break;
     }
 }
